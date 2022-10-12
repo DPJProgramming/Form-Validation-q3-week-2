0b79cc047caee0dbf360176d11b7f2ac9b6fbbc1
cheesy d <cheesyd64@hotmail.com>
2022-10-11 22:16:20 -0700

    Added validation for textboxes

--- js/main.js
+++ js/main.js
@@ -3,5 +3,15 @@ window.onload = function () {
     formButton.onclick = main;
 };
 function main() {
-    alert("button clicked");
+    isTextPresent("first-name", "First name is required");
+    isTextPresent("last-name", "Last name is required");
+}
+function isTextPresent(id, errorMessage) {
+    var textBox = document.getElementById(id);
+    var textBoxValue = textBox.value;
+    if (textBoxValue == "") {
+        var errorSpan = textBox.nextElementSibling;
+        errorSpan.innerHTML = errorMessage;
+        return false;
+    }
 }
--- ts/main.ts
+++ ts/main.ts
@@ -5,5 +5,28 @@ window.onload = function(){
 }
 
 function main():void{
-    alert("button clicked");
+    isTextPresent("first-name", "First name is required");
+
+    isTextPresent("last-name", "Last name is required");
+
+
+
+}
+
+/**
+ * returns true if there is text in textbo with given id
+ * @param id id of <inout type="text"
+ * @param errorMessage displays in sibling span of said textbox
+ * @returns true or false
+ */
+function isTextPresent(id:string, errorMessage:string):boolean {
+    let textBox = <HTMLInputElement>document.getElementById(id);
+    let textBoxValue = textBox.value;
+
+    if (textBoxValue == "") {
+        let errorSpan: HTMLElement = <HTMLElement>textBox.nextElementSibling;
+        errorSpan.innerHTML = errorMessage ;
+
+        return false;
+    }
 }
